bd3463c28b009eb8501166dce7db44d05bc91ed8
kdming <301253771@qq.com>
2021-08-24 14:44:47 +0800

    添加启动参数appPort

--- common/config/config.go
+++ common/config/config.go
@@ -9,28 +9,22 @@ import (
 )
 
 type Config struct {
-	DB_HOST   string `yaml:"DB_HOST"`
-	DB_USER   string `yaml:"DB_USER"`
-	DB_PWD    string `yaml:"DB_PWD"`
-	DB_NAME   string `yaml:"DB_NAME"`
-	Token_KEY string `yaml:"Token_KEY"`
-	HttpUrl   string `yaml:"HttpUrl"`
+	DB_HOST  string `yaml:"DB_HOST"`
+	DB_USER  string `yaml:"DB_USER"`
+	DB_PWD   string `yaml:"DB_PWD"`
+	DB_NAME  string `yaml:"DB_NAME"`
+	TOKENKEY string `yaml:"TOKENKEY"`
 }
 
 func GetConfig() Config {
-
-	// 设置文件路径
 	root, err := os.Getwd()
 	filePath := root + "/config.yaml"
 
-	// 读取并解析文件
 	buffer, err := ioutil.ReadFile(filePath)
 	config := Config{}
 	err = yaml.Unmarshal(buffer, &config)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	return config
-
 }
