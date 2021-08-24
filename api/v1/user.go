f88e45ffc625eeea2969c173d9a2c161e15a7d8a
kdming <301253771@qq.com>
2021-08-24 15:20:40 +0800

    删除models层逻辑代码，交于service层

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,51 +1,41 @@
 package v1
 
 import (
+	"app/common/app"
 	"app/models"
-	"app/pkg/e"
-	"app/service/user_service"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Register(c *gin.Context) {
-
 	user := &models.User{}
 	if err := c.ShouldBind(user); err != nil {
-		e.Err("bind", err)
+		app.Err("bind", err)
 	}
 
-	if err := user.Save(); err != nil {
-		e.Err("注册失败", err)
+	if err := userSvc.Register(user); err != nil {
+		app.Err("注册失败", err)
 	}
 
-	tokenSvc := user_service.TokenSvc{}
 	token, err := tokenSvc.MakeToken(user)
 	if err != nil {
-		e.Err("token生成失败", err)
+		app.Err("token生成失败", err)
 	}
-
-	e.Ok(c, "注册成功", e.Map{"token": token})
-
+	app.Ok(c, "注册成功", app.Map{"token": token})
 }
 
 func Login(c *gin.Context) {
-
 	user := &models.User{}
 	if err := c.ShouldBind(user); err != nil {
-		e.Err("bind", err)
+		app.Err("bind", err)
 	}
 
-	if err := user.Login(); err != nil {
-		e.Err("登录失败", err)
+	if err := userSvc.Login(user); err != nil {
+		app.Err("登录失败", err)
 	}
 
-	tokenSvc := user_service.TokenSvc{}
 	token, err := tokenSvc.MakeToken(user)
 	if err != nil {
-		e.Err("token生成失败", err)
+		app.Err("token生成失败", err)
 	}
-
-	e.Ok(c, "登录成功", e.Map{"token": token})
-
+	app.Ok(c, "登录成功", app.Map{"token": token})
 }
